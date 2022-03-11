bfb186d4399b943f23f6aa4d20bdb6e5c59b7728
Mehmet Bati <batimehmet84@gmail.com>
2022-03-11 16:21:45 +0300

    taslak deneme

--- docs/resume/download_avesis_cv.bat
+++ docs/resume/download_avesis_cv.bat
@@ -2,8 +2,8 @@
 @setlocal enableextensions
 @cd /d "%~dp0"
 
-set name="ugur"
-set surname="coruh"
+set name="mehmet"
+set surname="bati"
 
 curl -o "%name%_%surname%_tr_cv.pdf" "https://avesis.erdogan.edu.tr/%name%.%surname%/indir?languageCode=tr"
 curl -o "%name%_%surname%_en_cv.pdf" "https://avesis.erdogan.edu.tr/%name%.%surname%/indir?languageCode=en"
